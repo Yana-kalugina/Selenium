a68f815da6f5f292b58501583d5f9409ba1ab313
Yana Kalugina <yanakaluginayana@gmail.com>
2022-09-12 18:32:18 +0300

    Fail test

---
+++ src/test/java/ru/netology/web/CallbackTest.java
@@ -0,0 +1,41 @@
+package ru.netology.web;
+
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
+public class CallbackTest {
+    private WebDriver driver;
+
+    @BeforeAll
+    static void setUpAll() {
+        System.setProperty("webdriver.chrome.driver","driver/win/chromedriver.exe");
+    }
+
+    @BeforeEach
+    void setUp() {
+        driver = new ChromeDriver();
+    }
+
+    @AfterEach
+    void tearsDown() {
+        driver.quit();
+        driver=null;
+    }
+
+    @Test
+    void test() {
+        driver.get("http://localhost:9999/");
+        List<WebElement> elements = driver.findElements(By.className("input__control"));
+        elements.get(0).sendKeys("Калугина Яна");
+        elements.get(1).sendKeys("+79150447020");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.className("button__text")).click();
+        String text = driver.findElement(By.className("paragraph")).getText();
+        Assertions.assertEquals("ВАША заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
+    }
+}
